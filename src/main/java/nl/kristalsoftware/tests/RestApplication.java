14999b10f9606f146914a6a92ea57f6d947c8fd9
Sjoerd Adema <sjoerd@kristalsoftware.nl>
2017-12-17 22:10:22 +0100

    [develop] initial commit

---
+++ src/main/java/nl/kristalsoftware/tests/RestApplication.java
@@ -0,0 +1,8 @@
+package nl.kristalsoftware.tests;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/resources")
+public class RestApplication extends Application{
+}
